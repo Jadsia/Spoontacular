db90d5649bf60521b08d4336716cb36f0bd150cc
Jadsia <megankyoung@live.com>
2019-08-28 18:10:30 -0700

    added post function, still outputs in active record

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,22 @@
 class RecipesController < ApplicationController
 
   def index
-   # @recipes = Recipe.all
+   @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    Recipe.create(recipe_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :directions)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'recipes#index'
+  resource :recipes do
+    resources :recipes, only: :create
+  end
+  get 'about', to: 'recipes#about'
 end
